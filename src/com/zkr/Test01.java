7f52c67044db117cee5458f7742380a882764101
wgsizz <wgsizz@163.com>
2020-09-17 09:55:38 +0800

    02

--- src/com/zkr/Test01.java
+++ src/com/zkr/Test01.java
@@ -8,5 +8,6 @@ public class Test01 {
 
     public static void main(String[] args) {
         System.out.println("-----------------01");
+        System.out.println("-----------------02");
     }
 }
