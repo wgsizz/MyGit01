6ee9d427f130f245a17cfa5e76127ccb5e8e6ef7
wgsizz <wgsizz@163.com>
2020-09-17 09:53:26 +0800

    01

---
+++ src/com/zkr/Test01.java
@@ -0,0 +1,12 @@
+package com.zkr;
+
+/**
+ * 帅帅
+ * 2020/9/17
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("-----------------01");
+    }
+}
