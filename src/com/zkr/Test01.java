29995cf726708dab9acc5247a30b9d2e73394b8e
wgsizz <wgsizz@163.com>
2020-09-17 10:04:51 +0800

    04

--- src/com/zkr/Test01.java
+++ src/com/zkr/Test01.java
@@ -10,5 +10,6 @@ public class Test01 {
         System.out.println("-----------------01");
         System.out.println("-----------------02");
         System.out.println("-----------------03");
+        System.out.println("-----------------04");
     }
 }
